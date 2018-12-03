456e22a99c036f3326a426cf1a1948b98583f6be
James Furey <jfurey@gmail.com>
2018-12-03 13:46:24 +1100

    Refactored day 1 part 1 based on insight gained in part 2.

--- node/day-01/part-1.js
+++ node/day-01/part-1.js
@@ -2,17 +2,20 @@ const fs = require('fs');
 
 const input = fs.readFileSync('input.txt', 'utf-8');
 
-const changes = input.split('\n').filter(change => change.length);
+const changes = input
+  .split('\n')
+  .filter(change => change.length)
+  .map(change => {
+    const operator = change[0];
+    const value = change.slice(1) * 1;
+    return { operator, value };
+  });
 
 const operations = {
   '+': (x, y) => x + y,
   '-': (x, y) => x - y,
 };
 
-let frequency = changes.reduce((carry, change) => {
-  const operator = change[0];
-  const value = change.slice(1) * 1;
-  return operations[operator](carry, value);
-}, 0);
+const frequency = changes.reduce((carry, {operator, value}) => operations[operator](carry, value), 0);
 
 console.log(`frequency: ${frequency}`);
