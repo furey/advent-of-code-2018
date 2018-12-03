fbbe22eb9a0ffa2814848cc8c089e4242e80c021
James Furey <jfurey@gmail.com>
2018-12-03 14:07:16 +1100

    PHP day 1 part 1 solution.

---
+++ php/day-01/part-1.php
@@ -0,0 +1,20 @@
+<?php
+
+$input = file_get_contents('input.txt');
+
+$changes = array_map(function ($change) {
+    $operator = $change[0];
+    $value = substr($change, 1) * 1;
+    return compact('operator', 'value');
+}, array_filter(explode(PHP_EOL, $input)));
+
+$operations = [
+    '+' => function ($x, $y) { return $x + $y; },
+    '-' => function ($x, $y) { return $x - $y; },
+];
+
+$frequency = array_reduce($changes, function ($carry, $change) use ($operations) {
+    return $operations[$change['operator']]($carry, $change['value']);
+}, 0);
+
+echo "frequency: $frequency";
