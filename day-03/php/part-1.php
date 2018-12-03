0f6b8c439c7f2306eb27204c1fa75ec1be5e04d9
James Furey <jfurey@gmail.com>
2018-12-03 19:50:36 +1100

    Refactored day 3 PHP common logic into shared source file.

--- day-03/php/part-1.php
+++ day-03/php/part-1.php
@@ -1,36 +1,9 @@
 <?php
 
-$input = file_get_contents(__DIR__ . '/../input.txt');
+list($claims, $claimedFabric) = require(__DIR__ . '/src/shared.php');
 
-$claims = array_map(function ($claim) {
-    $matches = null;
-    preg_match('/^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/i', $claim, $matches);
-    return [
-        'claim' => $claim,
-        'id' => $matches[1],
-        'x' => $matches[2],
-        'y' => $matches[3],
-        'w' => $matches[4],
-        'h' => $matches[5],
-    ];
-}, array_filter(explode(PHP_EOL, $input)));
-
-$claimedFabric = array_reduce($claims, function ($carry, $claim) {
-    for ($x = $claim['x']; $x < $claim['x'] + $claim['w']; $x++) {
-        for ($y = $claim['y']; $y < $claim['y'] + $claim['h']; $y++) {
-            $key = "$x,$y";
-            if (isset($carry[$key])) {
-                $carry[$key]++;
-            } else {
-                $carry[$key] = 1;
-            }
-        }
-    }
-    return $carry;
-}, []);
-
-$sharedFabric = count(array_filter($claimedFabric, function ($count) {
-    return $count >= 2;
+$sharedFabric = count(array_filter($claimedFabric, function ($claimIds) {
+    return count($claimIds) >= 2;
 }));
 
 echo "shared fabric: $sharedFabric";
--- day-03/php/part-2.php
+++ day-03/php/part-2.php
@@ -1,54 +1,6 @@
 <?php
 
-$input = file_get_contents(__DIR__ . '/../input.txt');
-
-ini_set('memory_limit', -1);
-
-$data = array_map(function ($claim) {
-    $matches = null;
-    preg_match('/^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)/i', $claim, $matches);
-    $id = $matches[1];
-    return [
-        'key' => $id,
-        'values' => [
-            'id' => $matches[1],
-            'x' => $matches[2],
-            'y' => $matches[3],
-            'w' => $matches[4],
-            'h' => $matches[5],
-        ],
-    ];
-}, array_values(array_filter(explode(PHP_EOL, $input))));
-
-$keys = array_map(function ($item) {
-    return $item['key'];
-}, $data);
-
-$values = array_map(function ($item) {
-    return $item['values'];
-}, $data);
-
-unset($data);
-
-$claims = array_combine($keys, $values);
-
-unset($keys, $values);
-
-$claimedFabric = [];
-
-foreach ($claims as $id => $claim) {
-    for ($y = $claim['y']; $y < $claim['y'] + $claim['h']; $y++) {
-        for ($x = $claim['x']; $x < $claim['x'] + $claim['w']; $x++) {
-            $position = "$x,$y";
-            if (!isset($claimedFabric[$position])) $claimedFabric[$position] = [];
-            $claimedFabric[$position][] = $claim['id'];
-            if (count($claimedFabric[$position]) < 2) continue;
-            foreach ($claimedFabric[$position] as $id) $claims[$id]['overlaps'] = true;
-        }
-    }
-}
-
-unset($claimedFabric);
+list($claims, $claimedFabric) = require(__DIR__ . '/src/shared.php');
 
 $intactFabric = array_filter($claims, function ($claim) {
     return !isset($claim['overlaps']);
