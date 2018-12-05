3035d639aa9c67551bd4cfaf15c23e8ae886585a
James Furey <jfurey@gmail.com>
2018-12-05 18:37:32 +1100

    Refactored node day 5 part 1 based on learnings from part 2.

--- day-05/nodejs/part-1.js
+++ day-05/nodejs/part-1.js
@@ -1,22 +1,10 @@
 const fs = require('fs');
+const react = require('./src/react');
 
 const polymer = fs.readFileSync(__dirname + '/../input.txt', 'utf-8').trim();
 
-let units = polymer.split('');
+const unitsRemaining = react(polymer).length;
 
-let index = 0;
-while (true) {
-  if (index === units.length - 1) break;
-  a = units[index];
-  b = units[index + 1];
-  if (a.toUpperCase() === b.toUpperCase() && a !== b) {
-    units.splice(index, 2);
-    index = Math.max(0, index - 1);
-    continue;
-  }
-  index++;
-}
-
-console.log(`units remaining: ${units.length}`);
+console.log(`units remaining: ${unitsRemaining}`);
 
 
