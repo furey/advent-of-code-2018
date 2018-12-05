af1b8de5460ea60a5340971c5cd9257b1cc36424
James Furey <jfurey@gmail.com>
2018-12-05 19:03:37 +1100

    Refactored day 5 "react" method for speed (manipulating strings instead of arrays).

--- day-05/php/src/react.php
+++ day-05/php/src/react.php
@@ -1,18 +1,17 @@
 <?php
 
 return function ($polymer) {
-    $units = str_split($polymer);
     $index = 0;
     while (true) {
-        if ($index === count($units) - 1) break;
-        $a = $units[$index];
-        $b = $units[$index + 1];
+        if ($index === strlen($polymer) - 1) break;
+        $a = $polymer[$index];
+        $b = $polymer[$index + 1];
         if (strtoupper($a) === strtoupper($b) && $a !== $b) {
-            array_splice($units, $index, 2);
+            $polymer = preg_replace("/$a$b/", '', $polymer, 1);
             $index = max(0, $index - 1);
             continue;
         }
         $index++;
     }
-    return implode('', $units);
+    return $polymer;
 };
