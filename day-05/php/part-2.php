e06f91002fff032224fb16dd940a5a74655bd28a
James Furey <jfurey@gmail.com>
2018-12-05 19:20:10 +1100

    PHP day 5 part 2 solution.

---
+++ day-05/php/part-2.php
@@ -0,0 +1,20 @@
+<?php
+
+$react = require(__DIR__ . '/src/react.php');
+$sortBy = require(__DIR__ . '/src/sortBy.php');
+
+$polymer = trim(file_get_contents(__DIR__ . '/../input.txt'));
+
+$results = array_map(function ($type) use ($polymer, $react) {
+    $typeless = preg_replace("/$type/i", '', $polymer);
+    $length = strlen($react($typeless));
+    return compact('type', 'length');
+}, str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+
+$sortBy($results, function ($result) {
+    return $result['length'];
+});
+
+$shortest = $results[0];
+
+echo "length of the shortest polymer ('{$shortest['type']}'): {$shortest['length']}";
