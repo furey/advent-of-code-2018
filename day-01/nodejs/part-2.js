2deb780b1f13c6803a86c70447cceb6e031c4664
James Furey <jfurey@gmail.com>
2018-12-05 19:49:44 +1100

    Refactored node day 1 part 2 for speed.

--- day-01/nodejs/part-2.js
+++ day-01/nodejs/part-2.js
@@ -1,18 +1,18 @@
 const {changes, operations} = require(__dirname + '/src/shared');
 
 let frequency = 0;
-const frequencies = [];
+const frequencies = {};
 let firstFrequencyReachedTwice = null;
 
 while (firstFrequencyReachedTwice === null) {
   for (var i = 0; i < changes.length; i++) {
     const {operator, value} = changes[i];
     frequency = operations[operator](frequency, value);
-    if (frequencies.includes(frequency)) {
+    if (frequencies[frequency] !== undefined) {
       firstFrequencyReachedTwice = frequency;
       break;
     }
-    frequencies.push(frequency);
+    frequencies[frequency] = true;
   }
 }
 
