1789c0513e1651e29fe4f9bbd112251e28d196eb
James Furey <jfurey@gmail.com>
2018-12-11 18:14:01 +1100

    Refactors for clarity.

--- day-10/nodejs/part-2.js
+++ day-10/nodejs/part-2.js
@@ -10,19 +10,13 @@ let points = input
   .map(line => {
     const matches = line.match(/^position=<([ -]*\d+), ([ -]*\d+)\> velocity=<([ -]*\d+), ([ -]*\d+)>$/);
     return {
-      pos: {
-        x: matches[1] * 1,
-        y: matches[2] * 1,
-      },
-      vel: {
-        x: matches[3] * 1,
-        y: matches[4] * 1,
-      }
+      pos: { x: +matches[1], y: +matches[2] },
+      vel: { x: +matches[3], y: +matches[4] },
     }
     return matches;
   });
 
-function step(points) {
+function tick() {
   points = points.map(p => {
     p.pos.x += p.vel.x;
     p.pos.y += p.vel.y;
@@ -35,54 +29,43 @@ function step(points) {
     y: [Math.min(...y), Math.max(...y)],
   }
   const width = Math.abs(bounds.x[1] - bounds.x[0]);
-  return { points, bounds, width };
+  return {points, bounds, width};
 }
 
-async function tick() {
-  let second = 0;
-  let bounds, width;
-  while (true) {
-    const nextPoints = points.slice(0).map(p => {
-      p.pos.x += p.vel.x;
-      p.pos.y += p.vel.y;
-      return p;
-    });
-    const x = nextPoints.map(p => p.pos.x);
-    const y = nextPoints.map(p => p.pos.y);
-    const nextBounds = {
-      x: [Math.min(...x), Math.max(...x)],
-      y: [Math.min(...y), Math.max(...y)],
-    }
-    const nextWidth = Math.abs(nextBounds.x[1] - nextBounds.x[0]);
-    if (nextWidth >= width) {
-      let output = '';
-      const o = points.map(p => {
-        p.pos.x -= p.vel.x;
-        p.pos.y -= p.vel.y;
-        return p;
-      }).reduce((carry, point) => {
-        let {x, y} = point.pos;
-        const key = `${x},${y}`;
-        carry[key] = true;
-        return carry;
-      }, {});
-      for (let row = bounds.y[0]; row <= bounds.y[1]; row++) {
-        for (let col = bounds.x[0]; col <= bounds.x[1]; col++) {
-          const key = `${col},${row}`;
-          const pos = { x: col, y: row };
-          output += o[key] !== undefined ? '#' : '.';
-        }
-        output += '\n';
-      }
-      console.log(output);
-      console.log(`seconds needed to wait: ${second}`);
-      break;
-    }
-    points = nextPoints;
-    bounds = nextBounds;
-    width = nextWidth;
-    second++;
+let second = 0;
+let bounds = undefined;
+let width = Infinity;
+
+while (true) {
+  const next = tick();
+  if (next.width < width) {
+      points = next.points;
+      bounds = next.bounds;
+      width = next.width;
+      second++;
+      continue;
   }
+  const o = points.map(p => {
+    p.pos.x -= p.vel.x;
+    p.pos.y -= p.vel.y;
+    return p;
+  }).reduce((carry, point) => {
+    let {x, y} = point.pos;
+    const key = `${x},${y}`;
+    carry[key] = true;
+    return carry;
+  }, {});
+  let output = '';
+  for (let row = bounds.y[0]; row <= bounds.y[1]; row++) {
+    for (let col = bounds.x[0]; col <= bounds.x[1]; col++) {
+      const key = `${col},${row}`;
+      const pos = { x: col, y: row };
+      output += o[key] !== undefined ? '#' : '.';
+    }
+    output += '\n';
+  }
+  console.log(output);
+  break;
 }
 
-tick();
+console.log(`seconds needed to wait: ${second}`);
