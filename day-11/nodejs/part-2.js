88a92f8967a7c36b891d6954e6cc2291371a7170
James Furey <jfurey@gmail.com>
2018-12-13 12:25:23 +1100

    Caching already calculated squares for speed.

--- day-11/nodejs/part-2.js
+++ day-11/nodejs/part-2.js
@@ -24,26 +24,41 @@ for (let row = 1; row <= gridSize; row++) {
   }
 }
 
+let squares = new Map;
 let size = 1;
 let largestTotalPower = 0;
-let key, identifier;
+let largestIdentifier;
 while (size <= gridSize) {
   for (let row = 1; row <= gridSize - (size - 1); row++) {
     for (let col = 1; col <= gridSize - (size - 1); col++) {
-      key = `${col},${row},${size}`;
-      totalPower = 0;
-      for (var i = 0; i <= size - 1; i++) {
-        for (var j = 0; j <= size - 1; j++) {
-          totalPower += cells[row+i][col+j];
+      const key = `${col},${row},${size}`;
+      const prevKey = `${col},${row},${size - 1}`;
+      let totalPower = 0;
+      if (squares.has(prevKey)) {
+        totalPower = squares.get(prevKey);
+        const rightCol = col + (size - 1);
+        const bottomRow = row + (size - 1);
+        for (let bottomCol = col; bottomCol <= rightCol; bottomCol++) {
+          totalPower += cells[bottomRow][bottomCol];
+        }
+        for (let rightRow = row; rightRow <= row + (size - 2); rightRow++) {
+          totalPower += cells[rightRow][rightCol];
+        }
+      } else {
+        for (var i = 0; i <= size - 1; i++) {
+          for (var j = 0; j <= size - 1; j++) {
+            totalPower += cells[row + i][col + j];
+          }
         }
       }
+      squares.set(key, totalPower);
       if (totalPower >= largestTotalPower) {
         largestTotalPower = totalPower;
-        identifier = key;
+        largestIdentifier = key;
       }
     }
   }
   size++;
 }
 
-console.log(`X,Y,size identifier of the square with the largest total power: ${identifier}`);
+console.log(`X,Y,size identifier of the square with the largest total power: ${largestIdentifier} (${largestTotalPower})`);
